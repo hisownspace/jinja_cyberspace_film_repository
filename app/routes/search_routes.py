a06c3e869186328ab61ca4593f2ca470e47d2a16
David Nash <hisownspace@gmail.com>
2023-03-07 17:50:10 -0500

    create search suggestions page

--- app/routes/search_routes.py
+++ app/routes/search_routes.py
@@ -18,4 +18,44 @@ def search():
     return redirect(f"/films/{film.id}")
   
   else:
-    return render_template("no_search_results.html", query=query)
+    actors = Actor.query.all()
+    films = Film.query.all()
+    print(films)
+    film_queries = {}
+    actor_queries = {}
+    for actor in actors:
+      check = check_search(query, actor.name.lower(), actor.id)
+      if check:
+        actor_queries[actor.id] = actor.name
+    for film in films:
+      check = check_search(query, film.title.lower(), film.id)
+      if check:
+        film_queries[film.id] = film.title
+    return render_template("no_search_results.html", query=query, actor_queries=actor_queries, film_queries=film_queries)
+  
+  
+def check_search(query, name, id):
+    # print(name, id)
+    if query[:5] == name[:5]:
+      return True
+    count = 0
+    query_copy = query
+    for char in name:
+      if char in query_copy:
+        count += 1
+        idx = query_copy.find(char)
+        query_copy = query_copy[:idx] + (query_copy[idx+1:] if idx < len(query_copy) else "")
+    if count / len(name) > .90 and abs(len(query)-len(name)) < 5 :
+      print("jumble-match", name)
+      return True
+    start = 0
+    matches = 0
+    while start + 4 <= len(query):
+      if query[start:start+4] in name:
+        matches += 1
+        print(name, query[start:start+4])
+      start += 1
+    if matches >= 3:
+      print("4-letter match", name)
+      return True
+    return False
